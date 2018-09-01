359f31c3f456cf52550496d30e14a487d75a7100
jere99 <jrdegreeff@gmail.com>
2018-09-01 18:50:49 -0400

    add command line options

--- configureGit.sh
+++ configureGit.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 
+usage() {
+    echo "usage: configureGit [[[-f file] [-o]] | [-h]]"
+}
+
+filename=~/.gitignore_global
+overwrite=
+while [ "$1" != "" ]
+do
+    case $1 in
+        -f | --file)
+            shift
+            filename=$1
+            ;;
+        -o | --overwrite)
+            overwrite=1
+            ;;
+        -h | --help)
+            usage
+            exit
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
+
 echo Setting up git configuration.
 
-git config --global core.excludesfile ~/.gitignore_global
-echo -e '# Mac\n.DS_Store' > ~/.gitignore_global
+git config --global core.excludesfile $filename
+
+if [ "$overwrite"  = "1" ] || [ ! -f $filename ]
+then
+    echo -e "# Mac\n.DS_Store" > $filename
+fi
 
 echo Complete.
